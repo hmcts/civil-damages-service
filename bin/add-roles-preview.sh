6aab35ab148aa315b3a8e0fb52b0dc65f5c11cf0
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2021-02-08 11:16:31 +0000

    pulled docker scripts to bin directory

--- bin/add-roles-preview.sh
+++ bin/add-roles-preview.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # User used during the CCD import and ccd-role creation
-./civil-unspecified-docker/bin/utils/ccd-add-role.sh "caseworker-civil"
-./civil-unspecified-docker/bin/utils/ccd-add-role.sh "caseworker-caa"
-./civil-unspecified-docker/bin/utils/ccd-add-role.sh "caseworker-approver"
+./bin/utils/ccd-add-role.sh "caseworker-civil"
+./bin/utils/ccd-add-role.sh "caseworker-caa"
+./bin/utils/ccd-add-role.sh "caseworker-approver"
 
 roles=("solicitor" "systemupdate")
 for role in "${roles[@]}"
 do
-  ./civil-unspecified-docker/bin/utils/ccd-add-role.sh "caseworker-civil-${role}"
+  ./bin/utils/ccd-add-role.sh "caseworker-civil-${role}"
 done
