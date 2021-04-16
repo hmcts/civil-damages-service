410f83e4f63020e772bd912efdef3944fcc121b2
Dharmendra kumar <k_dharmendra@hotmail.com>
2021-04-16 10:52:34 +0100

    default the product in main

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.component}-${var.env}"
+  name     = "unspec-${var.component}-${var.env}"
   location = var.location
 
   tags = var.common_tags
@@ -12,7 +12,7 @@ resource "azurerm_resource_group" "rg" {
 module "key-vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                    = "unspec-${var.env}"
-  product                 = var.product
+  product                 = "unspec"
   env                     = var.env
   tenant_id               = var.tenant_id
   object_id               = var.jenkins_AAD_objectId
@@ -23,16 +23,16 @@ module "key-vault" {
 }
 
 locals {
-  vaultName = "${var.product}-${var.env}"
+  vaultName = "unspec-${var.env}"
 }
 
 data "azurerm_key_vault" "damages_key_vault" {
   name = local.vaultName
-  resource_group_name = "${var.product}-service-${var.env}"
+  resource_group_name = "unspec-service-${var.env}"
 }
 
 resource "azurerm_application_insights" "appinsights" {
-  name                = "${var.product}-${var.component}-appinsights-${var.env}"
+  name                = "unspec-${var.component}-appinsights-${var.env}"
   location            = var.appinsights_location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
