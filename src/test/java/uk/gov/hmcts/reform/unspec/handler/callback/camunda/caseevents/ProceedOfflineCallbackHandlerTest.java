d4997be9dee4b6ed32b01f65e53cb110742b30dd
Dharmendra kumar <k_dharmendra@hotmail.com>
2021-04-22 15:12:21 +0100

    complete the test

--- src/test/java/uk/gov/hmcts/reform/unspec/handler/callback/camunda/caseevents/ProceedOfflineCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/unspec/handler/callback/camunda/caseevents/ProceedOfflineCallbackHandlerTest.java
@@ -1,7 +1,39 @@
 package uk.gov.hmcts.reform.unspec.handler.callback.camunda.caseevents;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.unspec.callback.CallbackParams;
+import uk.gov.hmcts.reform.unspec.callback.CallbackType;
+import uk.gov.hmcts.reform.unspec.handler.callback.BaseCallbackHandlerTest;
+import uk.gov.hmcts.reform.unspec.helpers.CaseDetailsConverter;
+import uk.gov.hmcts.reform.unspec.model.CaseData;
+import uk.gov.hmcts.reform.unspec.sampledata.CaseDataBuilder;
 
-class ProceedOfflineCallbackHandlerTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest(classes = {
+    ProceedOfflineCallbackHandler.class,
+    JacksonAutoConfiguration.class,
+    CaseDetailsConverter.class
+})
+class ProceedOfflineCallbackHandlerTest extends BaseCallbackHandlerTest {
+
+    @Autowired
+    ProceedOfflineCallbackHandler proceedOfflineCallbackHandler;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void shouldCaptureTakenOfflineDate() {
+        CaseData caseData = CaseDataBuilder.builder().atStatePendingClaimIssuedUnRepresentedDefendant().build();
+        CallbackParams params = callbackParamsOf(caseData, CallbackType.ABOUT_TO_SUBMIT);
+
+        var response = (AboutToStartOrSubmitCallbackResponse) proceedOfflineCallbackHandler.handle(params);
+
+        assertThat(response.getData()).extracting("takenOfflineDate").isNotNull();
+    }
 }
