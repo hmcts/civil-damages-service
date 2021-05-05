8a5e0a22b554e161fca353a82b9b231b5a5277c3
douglasrice <douglasrice@yahoo.com>
2021-05-05 10:42:52 +0100

    Update ResubmitClaimCallbackHandlerTest.java

--- src/test/java/uk/gov/hmcts/reform/unspec/handler/callback/user/ResubmitClaimCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/unspec/handler/callback/user/ResubmitClaimCallbackHandlerTest.java
@@ -64,7 +64,7 @@ class ResubmitClaimCallbackHandlerTest extends BaseCallbackHandlerTest {
                 SubmittedCallbackResponse.builder()
                     .confirmationHeader("# Claim pending")
                     .confirmationBody("<br />Once your payment is confirmed your claim will be processed ready to be issued. "
-                                          + "Wait for us to contact you.")
+                    + "Wait for us to contact you.")
                     .build());
         }
     }
