631d08a6c65a92825274a01e325c79ecaf65b46d
Dharmendra kumar <k_dharmendra@hotmail.com>
2021-05-17 17:25:26 +0100

    ethos password copy

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -90,3 +90,24 @@ resource "azurerm_key_vault_secret" "unspec_db_password_secret" {
   value        = data.azurerm_key_vault_secret.db_password_secret.value
   key_vault_id = data.azurerm_key_vault.damages_key_vault.id
 }
+
+
+data "azurerm_key_vault" "ethos_vault" {
+  name                = "ethos-${var.env}"
+  resource_group_name = "ethos-repl-docmosis-backend-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "tornado_access_secret" {
+  key_vault_id = "${data.azurerm_key_vault.ethos_vault.id}"
+  name = "tornado-access-key"
+}
+
+resource "azurerm_key_vault_secret" "civil_docmosis_api_key" {
+  name         = "docmosis-api-key"
+  value        = data.azurerm_key_vault_secret.tornado_access_secret.value
+  key_vault_id = data.azurerm_key_vault.damages_key_vault.id
+
+  depends_on = [
+    module.key-vault
+  ]
+}
