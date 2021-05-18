eebe4c276a965ea00b88e7b37cf13feed2bd4299
dharmendra kumar <dharmendra.kumar@hmcts.net>
2021-05-18 20:01:21 +0100

    feat:add missing activity id in camuna event handlers (#147)

--- src/test/java/uk/gov/hmcts/reform/unspec/handler/callback/camunda/caseevents/ProceedOfflineCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/unspec/handler/callback/camunda/caseevents/ProceedOfflineCallbackHandlerTest.java
@@ -1,34 +1,75 @@
 package uk.gov.hmcts.reform.unspec.handler.callback.camunda.caseevents;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.unspec.callback.CallbackParams;
-import uk.gov.hmcts.reform.unspec.callback.CallbackType;
 import uk.gov.hmcts.reform.unspec.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.unspec.model.CaseData;
 import uk.gov.hmcts.reform.unspec.sampledata.CaseDataBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.unspec.callback.CallbackType.ABOUT_TO_SUBMIT;
 
 @SpringBootTest(classes = {
-    ProceedOfflineCallbackHandler.class,
+    ProceedOfflineForUnRepresentedCallbackHandler.class,
+    ProceedOfflineForUnRegisteredCallbackHandler.class,
     JacksonAutoConfiguration.class
 })
 class ProceedOfflineCallbackHandlerTest extends BaseCallbackHandlerTest {
 
-    @Autowired
-    ProceedOfflineCallbackHandler handler;
+    @Nested
+    class UnregisteredCallback {
 
-    @Test
-    void shouldCaptureTakenOfflineDate_whenProceedInHeritageSystemRequested() {
-        CaseData caseData = CaseDataBuilder.builder().atStatePendingClaimIssuedUnRepresentedDefendant().build();
-        CallbackParams params = callbackParamsOf(caseData, CallbackType.ABOUT_TO_SUBMIT);
+        @Autowired
+        ProceedOfflineForUnRegisteredCallbackHandler handler;
 
-        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+        @Test
+        void shouldCaptureTakenOfflineDate_whenProceedInHeritageSystemRequested() {
+            CaseData caseData = CaseDataBuilder.builder().atStatePendingClaimIssuedUnRepresentedDefendant().build();
+            CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
 
-        assertThat(response.getData()).extracting("takenOfflineDate").isNotNull();
+            var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+
+            assertThat(response.getData()).extracting("takenOfflineDate").isNotNull();
+        }
+
+        @Test
+        void shouldReturnCorrectActivityId_whenRequested() {
+            CaseData caseData = CaseDataBuilder.builder().atStateClaimSubmitted().build();
+
+            CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+
+            assertThat(handler.camundaActivityId(params)).isEqualTo("ProceedOfflineForUnregisteredFirm");
+        }
+    }
+
+    @Nested
+    class UnrepresentedCallback {
+
+        @Autowired
+        ProceedOfflineForUnRepresentedCallbackHandler handler;
+
+        @Test
+        void shouldCaptureTakenOfflineDate_whenProceedInHeritageSystemRequested() {
+            CaseData caseData = CaseDataBuilder.builder().atStatePendingClaimIssuedUnRepresentedDefendant().build();
+            CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+
+            var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+
+            assertThat(response.getData()).extracting("takenOfflineDate").isNotNull();
+        }
+
+        @Test
+        void shouldReturnCorrectActivityId_whenRequested() {
+            CaseData caseData = CaseDataBuilder.builder().atStateClaimSubmitted().build();
+
+            CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+
+            assertThat(handler.camundaActivityId(params)).isEqualTo("ProceedOfflineForUnRepresentedSolicitor");
+        }
     }
 }
